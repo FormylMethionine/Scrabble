ca4cdd4f6bf917863423595d04cb6d74e93475f3
Vincent Le Goff <vb.legoff@free.fr>
2020-02-10 21:19:29 +0100

    New function generageBag

--- randstring.py
+++ randstring.py
@@ -194,7 +194,41 @@ def generateBag():
 def NewgenerateBag():
 
     bag = []
-    table = {
+    table = {'A': 9,
+             'B': 2,
+             'C': 2,
+             'D': 3,
+             'E': 15,
+             'F': 2,
+             'G': 2,
+             'H': 2,
+             'I': 8,
+             'J': 1,
+             'K': 1,
+             'L': 5,
+             'M': 3,
+             'N': 6,
+             'O': 6,
+             'P': 2,
+             'Q': 1,
+             'R': 6,
+             'S': 6,
+             'T': 6,
+             'U': 6,
+             'V': 2,
+             'W': 1,
+             'X': 1,
+             'Y': 1,
+             'Z': 1,
+             '1': 2
+             }
+
+    for letter in table:
+        for i in range(table[letter]):
+            bag.append(letter)
+
+    return bag
+
 
 def tirage(liste, size):
 
@@ -205,3 +239,7 @@ def tirage(liste, size):
         ret += liste.pop(index)
 
     return ret, liste
+
+
+if __name__ == "__main__":
+    print(generateBag() == NewgenerateBag())
