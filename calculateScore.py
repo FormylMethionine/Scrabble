119a4c8289f9af0df5b80a7fbd88f9d3b773c9e8
Vincent Le Goff <vb.legoff@free.fr>
2020-02-10 19:08:17 +0100

    Function score rewritten + made files PEP8 compliants

--- calculateScore.py
+++ calculateScore.py
@@ -1,67 +1,110 @@
 def score(string):
 
-    score=0
+    score = 0
 
     for letter in string:
-       
-        if letter=='A':
-            score+=1
-        elif letter=='B':
-            score+=3
-        elif letter=='C':
-            score+=3
-        elif letter=='D':
-            score+=2
-        elif letter=='E':
-            score+=1
-        elif letter=='F':
-            score+=4
-        elif letter=='G':
-            score+=2
-        elif letter=='H':
-            score+=4
-        elif letter=='I':
-            score+=1
-        elif letter=='J':
-            score+=8
-        elif letter=='K':
-            score+=10
-        elif letter=='L':
-            score+=1
-        elif letter=='M':
-            score+=2
-        elif letter=='N':
-            score+=1
-        elif letter=='O':
-            score+=1
-        elif letter=='P':
-            score+=3
-        elif letter=='Q':
-            score+=8
-        elif letter=='R':
-            score+=1
-        elif letter=='S':
-            score+=1
-        elif letter=='T':
-            score+=1
-        elif letter=='U':
-            score+=1
-        elif letter=='V':
-            score+=4
-        elif letter=='W':
-            score+=10
-        elif letter=='X':
-            score+=10
-        elif letter=='Y':
-            score+=10
-        elif letter=='Z':
-            score+=10
-        elif letter=='1':  #joker (en attendant d'enlever le else suivant)
-            score+=0
-        else: #check les erreurs dans la string, utile pour vérifier le tirage, à retirer dans la VF
+
+        if letter == 'A':
+            score += 1
+        elif letter == 'B':
+            score += 3
+        elif letter == 'C':
+            score += 3
+        elif letter == 'D':
+            score += 2
+        elif letter == 'E':
+            score += 1
+        elif letter == 'F':
+            score += 4
+        elif letter == 'G':
+            score += 2
+        elif letter == 'H':
+            score += 4
+        elif letter == 'I':
+            score += 1
+        elif letter == 'J':
+            score += 8
+        elif letter == 'K':
+            score += 10
+        elif letter == 'L':
+            score += 1
+        elif letter == 'M':
+            score += 2
+        elif letter == 'N':
+            score += 1
+        elif letter == 'O':
+            score += 1
+        elif letter == 'P':
+            score += 3
+        elif letter == 'Q':
+            score += 8
+        elif letter == 'R':
+            score += 1
+        elif letter == 'S':
+            score += 1
+        elif letter == 'T':
+            score += 1
+        elif letter == 'U':
+            score += 1
+        elif letter == 'V':
+            score += 4
+        elif letter == 'W':
+            score += 10
+        elif letter == 'X':
+            score += 10
+        elif letter == 'Y':
+            score += 10
+        elif letter == 'Z':
+            score += 10
+        elif letter == '1':  # joker (en attendant d'enlever le else suivant)
+            score += 0
+        else:  # check les erreurs dans la string, utile pour vérifier le tirage, à retirer dans la VF
             print("wtf bro")
-            break;
+            break
 
     return score
 
-print(score("YEUX"))
+
+def scoreFinal(string):
+
+    score = 0
+    table = {'A': 1,
+             'B': 3,
+             'C': 3,
+             'D': 2,
+             'E': 1,
+             'F': 4,
+             'G': 2,
+             'H': 4,
+             'I': 1,
+             'J': 8,
+             'K': 10,
+             'L': 1,
+             'M': 2,
+             'N': 1,
+             'O': 1,
+             'P': 3,
+             'Q': 8,
+             'R': 1,
+             'S': 1,
+             'T': 1,
+             'U': 1,
+             'V': 4,
+             'W': 10,
+             'X': 10,
+             'Y': 10,
+             'Z': 10,
+             '1': 0
+             }
+
+    for letter in string:
+        if letter in table:
+            score += table[letter]
+        else:
+            print("wtf bro")
+            break
+
+    return score
+
+
+print(score("YEUX") == scoreFinal("YEUX"))
--- scrabble.py
+++ scrabble.py
@@ -1,93 +1,99 @@
-from randstring import randstring,generateBag,tirage
+from randstring import randstring, generateBag, tirage
 import time
+
 
 def list_from_file(name):
 
-    with open(name,"r") as f:
-        liste=f.read()
-        liste=liste.split()
+    with open(name, "r") as f:
+        liste = f.read()
+        liste = liste.split()
 
     return liste
 
-def regroup(listOfWords,i):
 
-    dic={}
+def regroup(listOfWords, i):
+
+    dic = {}
     for word in listOfWords:
-        if i>=len(word):
-            dic[0]=word
+        if i >= len(word):
+            dic[0] = word
         elif word[i] not in dic:
-            dic[word[i]]=[word]
+            dic[word[i]] = [word]
         else:
             dic[word[i]].append(word)
 
     return dic
 
-def make_dic(liste,i):
 
-    ret=liste
+def make_dic(liste, i):
 
-    ret=regroup(ret,i)
-    i+=1
+    ret = liste
+
+    ret = regroup(ret, i)
+    i += 1
     for letter in ret:
-        if type(letter)!=int:
-            ret[letter]=make_dic(ret[letter],i)
+        if type(letter) != int:
+            ret[letter] = make_dic(ret[letter], i)
 
     return ret
 
+
 def permutlist(string):
 
-    p=[[c for c in string]]
-    ret=set()
+    p = [[c for c in string]]
+    ret = set()
     ret.add(string)
 
     for k in range(len(string)-1):
         for i in range(len(p)):
-            perm=p[i][:]
+            perm = p[i][:]
             for j in range(len(string)-k-1):
                 perm.append(perm.pop(k))
                 p.append(perm[:])
 
     for i in p:
-        string=""
+        string = ""
         for j in i:
-            string+=j
+            string += j
         if string not in ret:
             ret.add(string)
 
     return ret
 
-def search_dic(dic,letters,base=""):
+
+def search_dic(dic, letters, base=""):
 
     for letter in base:
-        dic=dic[letter]
+        dic = dic[letter]
 
-    letters=permutlist(letters)
-    ret=set()
-    #print(letters)
+    letters = permutlist(letters)
+    ret = set()
+    # print(letters)
     for word in letters:
-        srcdic={i:dic[i] for i in dic}
+        srcdic = {i: dic[i] for i in dic}
         for letter in word:
-            #penser à ajouter gestions des jokers: 
-            #if letter==joker:
+            # penser à ajouter gestions des jokers:
+            # if letter==joker:
             #   for i in "alphabet":
             #       search
             if 0 in srcdic and srcdic[0] not in ret:
                 ret.add(srcdic[0])
             if letter in srcdic:
-                srcdic=srcdic[letter]
+                srcdic = srcdic[letter]
 
     return ret
 
-t0=time.perf_counter()
-liste=list_from_file("liste.txt")
+
+t0 = time.perf_counter()
+liste = list_from_file("liste.txt")
 print(len(liste))
-dic=(make_dic(liste,0))
-bag=generateBag()
-letters,Bag=tirage(bag,7)
+dic = (make_dic(liste, 0))
+bag = generateBag()
+letters, Bag = tirage(bag, 7)
 print(letters)
-#print(search_dic(dic,"EYNPFVZ"))
-playable=(search_dic(dic,letters))
+# print(search_dic(dic,"EYNPFVZ"))
+playable = (search_dic(dic, letters))
 print(playable, len(playable))
-t1=time.perf_counter()
+t1 = time.perf_counter()
 print(t1-t0)
-print(search_dic(dic,"ETIRAUPOA","TEST"))
+print(search_dic(dic, "ETIRAUPOA", "TEST"))
