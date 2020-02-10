f1108d77b6a68ac8425e2da4f34bb1da1a4657fa
Vincent Le Goff <vb.legoff@free.fr>
2020-02-10 21:20:46 +0100

    Removed outdated functions

--- randstring.py
+++ randstring.py
@@ -4,95 +4,6 @@ import numpy as np
 def randstring(n):
 
     string = ""
-
-    for i in range(n):
-
-        tirage = np.random.randint(26)
-
-        if tirage == 0:
-            string += "A"
-
-        elif tirage == 1:
-            string += "B"
-
-        elif tirage == 2:
-            string += "C"
-
-        elif tirage == 3:
-            string += "D"
-
-        elif tirage == 4:
-            string += "E"
-
-        elif tirage == 5:
-            string += "F"
-
-        elif tirage == 6:
-            string += "G"
-
-        elif tirage == 7:
-            string += "H"
-
-        elif tirage == 8:
-            string += "I"
-
-        elif tirage == 9:
-            string += "J"
-
-        elif tirage == 10:
-            string += "K"
-
-        elif tirage == 11:
-            string += "L"
-
-        elif tirage == 12:
-            string += "M"
-
-        elif tirage == 13:
-            string += "N"
-
-        elif tirage == 14:
-            string += "O"
-
-        elif tirage == 15:
-            string += "P"
-
-        elif tirage == 16:
-            string += "Q"
-
-        elif tirage == 17:
-            string += "R"
-
-        elif tirage == 18:
-            string += "S"
-
-        elif tirage == 19:
-            string += "T"
-
-        elif tirage == 20:
-            string += "U"
-
-        elif tirage == 21:
-            string += "V"
-
-        elif tirage == 22:
-            string += "W"
-
-        elif tirage == 23:
-            string += "X"
-
-        elif tirage == 24:
-            string += "Y"
-
-        elif tirage == 25:
-            string += "Z"
-
-    return string
-
-
-def Newrandstring(n):
-
-    string = ""
     table = {0: 'A',
              1: 'B',
              2: 'C',
@@ -127,68 +38,6 @@ def Newrandstring(n):
         string += table[tirage]
 
     return string
-
-
-def generateBag():
-
-    ret = []
-
-    for i in range(9):
-        ret += "A"
-    for i in range(2):
-        ret += "B"
-    for i in range(2):
-        ret += "C"
-    for i in range(3):
-        ret += "D"
-    for i in range(15):
-        ret += "E"
-    for i in range(2):
-        ret += "F"
-    for i in range(2):
-        ret += "G"
-    for i in range(2):
-        ret += "H"
-    for i in range(8):
-        ret += "I"
-    for i in range(1):
-        ret += "J"
-    for i in range(1):
-        ret += "K"
-    for i in range(5):
-        ret += "L"
-    for i in range(3):
-        ret += "M"
-    for i in range(6):
-        ret += "N"
-    for i in range(6):
-        ret += "O"
-    for i in range(2):
-        ret += "P"
-    for i in range(1):
-        ret += "Q"
-    for i in range(6):
-        ret += "R"
-    for i in range(6):
-        ret += "S"
-    for i in range(6):
-        ret += "T"
-    for i in range(6):
-        ret += "U"
-    for i in range(2):
-        ret += "V"
-    for i in range(1):
-        ret += "W"
-    for i in range(1):
-        ret += "X"
-    for i in range(1):
-        ret += "Y"
-    for i in range(1):
-        ret += "Z"
-    for i in range(2):
-        ret += "1"
-
-    return ret
 
 
 def NewgenerateBag():
@@ -239,7 +88,3 @@ def tirage(liste, size):
         ret += liste.pop(index)
 
     return ret, liste
-
-
-if __name__ == "__main__":
-    print(generateBag() == NewgenerateBag())
