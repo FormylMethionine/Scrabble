adfbc2cd46c4350e1659a15785208f6b66b02ca9
Charles Nodot <charles.nodot@insa-lyon.fr>
2020-08-09 17:49:48 +0200

    Ajout de la fonction existence_mot qui renvoie un booléen et qui permet de déterminer si un mot existe

--- scrabble.py
+++ scrabble.py
@@ -11,34 +11,39 @@ def list_from_file(name):
 
     return liste
 
-
+#A partir d'une liste de mots, on les compartimente en fonction d'une lettre à une place donnée.
 def regroup(listOfWords, i):
-
     dic = {}
     for word in listOfWords:
+#si le mot a la même longueur que l'indice, il est complètement décrit. La clé 0 indique que le chemin pour le trouver est complété.
         if i >= len(word):
             dic[0] = word
+#Si la clé-lettre word[i] n'existe pas, on l'ajoute et on y associe le mot.
         elif word[i] not in dic:
             dic[word[i]] = [word]
+#Si la clé-lettre existe déjà, on ajoute le mot à la liste de mots existants, associée à la même série de clés.
         else:
             dic[word[i]].append(word)
 
     return dic
 
-
+#crée un dictionnaire
 def make_dic(liste, i):
-
+#ret est la liste de mots initiale.
+#plus tard, ret devient une liste de mots associée à une clé. 
     ret = liste
-
+#premier tour, ret est devenu un dictionnaire
     ret = regroup(ret, i)
     i += 1
+#ret est devenu un dictionnaire, on va parcourir toutes les clés possibles. 
     for letter in ret:
         if type(letter) != int:
+#On crée un dictionnaire dans un dictionnaire, jusqu'à ce que tous les chemins soient parcourus et que toutes les clés des dictionnaires se terminent par 0.
             ret[letter] = make_dic(ret[letter], i)
 
     return ret
 
-
+#retourne une liste avec toutes les permutations possibles de la chaîne de caractères indiquée.
 def permutlist(string):
 
     p = [[c for c in string]]
@@ -61,7 +66,7 @@ def permutlist(string):
 
     return ret
 
-
+#paramètres utilisés : 
 def search_word(dic, word, ret, retlist, i):
 
     jokers = []
@@ -88,13 +93,33 @@ def search_dic(dic, letters, base=""):
     if base != "":
         for letter in base:
             dic = dic[letter]
-
+#ret est une liste vide.
     ret = set()
     wordlist = []
+#pour chaque suite de lettres ordonnée différemment à chaque fois
     for word in letters:
         search_word(dic, word, ret, wordlist, len(base))
 
     return wordlist
+
+
+def existence_mot(dic,letters):
+        b=False
+        ret=set()
+        wordlist=[]
+        search_word(dic, letters, ret, wordlist, 0)
+        if len(wordlist)==1:
+                if wordlist[0][0]==letters:
+                        b=True
+        elif len(wordlist)==0:
+                b=False
+        else:
+                for word in wordlist:
+                        if word==letters:
+                                b=True
+        return b
+
+
 
 
 liste = list_from_file("liste.txt")
@@ -102,8 +127,10 @@ print(len(liste))
 dic = (make_dic(liste, 0))
 bag = generateBag()
 letters, bag = tirage(bag, 7)
-# un bug fonctionnait avec SAREGHN, RSCEIRH
 print(letters)
+# un bug fonctionnait avec SAREGHN, RSCEIRH
+a=existence_mot(dic,"TRANQUILLE")
+print(a)
 t0 = time.perf_counter()
 playable = (search_dic(dic, letters))
 t1 = time.perf_counter()
